1102992f570409bcec1403091ffd097bcc8deb2f
n3t <n3t@n3t.cz>
2022-10-21 03:35:46 +0200

    User groups - add activate registration field

--- admin/update/sql/mysql/4.0.6.sql
+++ admin/update/sql/mysql/4.0.6.sql
@@ -2,3 +2,4 @@ ALTER TABLE `#__phocacart_categories` ADD COLUMN `title_long` varchar(255) NOT N
 ALTER TABLE `#__phocacart_products` ADD COLUMN `title_long` varchar(255) NOT NULL DEFAULT '';
 ALTER TABLE `#__phocacart_manufacturers` ADD COLUMN `title_long` varchar(255) NOT NULL DEFAULT '';
 ALTER TABLE `#__phocacart_products` ADD COLUMN `internal_comment` text;
+ALTER TABLE `#__phocacart_groups` ADD COLUMN `activate_registration` int(1) NOT NULL DEFAULT 0;
