571615c5c642fd63793ef3174cca36723f5e341d
n3t <n3t@n3t.cz>
2023-10-22 22:17:00 +0200

    Event onPCFonItemBeforeRender

---
+++ admin/libraries/src/Event/Feed/Item/BeforeRender.php
@@ -0,0 +1,20 @@
+<?php
+namespace Phoca\PhocaCart\Event\Feed\Item;
+
+use Joomla\CMS\Event\Result\ResultAware;
+use Joomla\CMS\Event\Result\ResultTypeBooleanAware;
+use Joomla\CMS\Form\Form;
+use Phoca\PhocaCart\Event\AbstractEvent;
+
+class BeforeRender extends AbstractEvent
+{
+  use ResultAware, ResultTypeBooleanAware;
+
+  public function __construct(string $context, string $feedName, Form $subForm) {
+    parent::__construct('pcf', 'onPCFonItemBeforeRender', [
+      'context' => $context,
+      'feedName' => $feedName,
+      'subForm' => &$subForm,
+    ]);
+  }
+}
