315c4a27e633a8ebef491f4b1035efb9110e68f5
n3t <n3t@n3t.cz>
2023-10-22 13:54:42 +0200

    New PhocaCart loaders style

--- admin/libraries/autoloadEscpos.php
+++ admin/libraries/autoloadEscpos.php
@@ -1,30 +1,21 @@
 <?php
-/**
- * Users who do not have 'composer' to manage dependencies, include this
- * file to provide auto-loading of the classes in this library. 
- * require JPATH_ADMINISTRATOR . '/components/com_phocacart/libraries/autoloadPhoca.php';
+/* @package Joomla
+ * @copyright Copyright (C) Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @extension Phoca Extension
+ * @copyright Copyright (C) Jan Pavelka www.phoca.cz
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
-spl_autoload_register ( function ($class) {
-	/*
-	 * PSR-4 autoloader, based on PHP Framework Interop Group snippet (Under MIT License.)
-	 * https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
-	 */
-	$prefix = "Mike42\\";
-	$base_dir = __DIR__ . "/";
-	
-	/* Only continue for classes in this namespace */
-	$len = strlen ( $prefix );
-	if (strncmp ( $prefix, $class, $len ) !== 0) {
-		return;
-	}
-	
-	/* Require the file if it exists */
-	$relative_class = substr ( $class, $len );
-	
-	$file = $base_dir . str_replace ( '\\', '/', $relative_class ) . '.php';
-	
-	
-	if (file_exists ( $file )) {
-		require $file;
-	}
-} );
+
+defined('_JEXEC') or die;
+
+trigger_error(
+  sprintf(
+    'Bootstrapping PhocaCart using the %1$s file is deprecated.  Use %2$s instead.',
+    __FILE__,
+    __DIR__ . '/bootstrap.php'
+  ),
+  E_USER_DEPRECATED
+);
+
+require_once __DIR__ . '/bootstrap.php';
