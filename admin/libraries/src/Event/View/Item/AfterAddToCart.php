80e7358c12f9639d5b783222a79231a4126fce62
n3t <n3t@n3t.cz>
2023-10-23 02:27:28 +0200

    Item is array, not object

--- admin/libraries/src/Event/View/Item/AfterAddToCart.php
+++ admin/libraries/src/Event/View/Item/AfterAddToCart.php
@@ -10,7 +10,7 @@ class AfterAddToCart extends AbstractEvent
 {
   use ResultAware, ResultTypeStringAware;
 
-  public function __construct(string $context, object $item, Registry $appParams) {
+  public function __construct(string $context, array $item, Registry $appParams) {
     parent::__construct('pcv', 'onPCVonItemAfterAddToCart', [
       'context' => $context,
       'item' => &$item,
--- admin/libraries/src/Event/View/Item/BeforeHeader.php
+++ admin/libraries/src/Event/View/Item/BeforeHeader.php
@@ -10,7 +10,7 @@ class BeforeHeader extends AbstractEvent
 {
   use ResultAware, ResultTypeStringAware;
 
-  public function __construct(string $context, object $item, Registry $appParams) {
+  public function __construct(string $context, array $item, Registry $appParams) {
     parent::__construct('pcv', 'onPCVonItemBeforeHeader', [
       'context' => $context,
       'item' => &$item,
