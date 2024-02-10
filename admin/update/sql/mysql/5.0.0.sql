b621eda700ad9375282a0f2e3ce1e1e1e3b70188
n3t <n3t@n3t.cz>
2024-02-10 02:43:24 +0100

    Parameters i18n

--- admin/update/sql/mysql/5.0.0.sql
+++ admin/update/sql/mysql/5.0.0.sql
@@ -166,3 +166,22 @@ CREATE TABLE IF NOT EXISTS `#__phocacart_tags_i18n` (
     PRIMARY KEY  (`id`, `language`),
     KEY `idx_alias` (`alias`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `#__phocacart_parameters_i18n` (
+    `id` int(11) NOT NULL,
+    `language` char(7) NOT NULL,
+    `title` varchar(255),
+    `alias` varchar(255),
+    `title_header` varchar(255),
+    PRIMARY KEY  (`id`, `language`),
+    KEY `idx_alias` (`alias`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `#__phocacart_parameter_values_i18n` (
+    `id` int(11) NOT NULL,
+    `language` char(7) NOT NULL,
+    `title` varchar(255),
+    `alias` varchar(255),
+    PRIMARY KEY  (`id`, `language`),
+    KEY `idx_alias` (`alias`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
--- admin/views/phocacartparametervalue/view.html.php
+++ admin/views/phocacartparametervalue/view.html.php
@@ -7,12 +7,12 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
 defined('_JEXEC') or die();
+
 use Joomla\CMS\MVC\View\HtmlView;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Toolbar\Toolbar;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Toolbar\ToolbarHelper;
-jimport( 'joomla.application.component.view' );
 
 class PhocaCartCpViewPhocacartParameterValue extends HtmlView
 {
@@ -31,17 +31,16 @@ class PhocaCartCpViewPhocacartParameterValue extends HtmlView
 		$this->form		= $this->get('Form');
 		$this->item		= $this->get('Item');
 
-		$media = new PhocacartRenderAdminmedia();
+		new PhocacartRenderAdminmedia();
 
 		$this->addToolbar();
 		parent::display($tpl);
 	}
 
 	protected function addToolbar() {
-
 		require_once JPATH_COMPONENT.'/helpers/'.$this->t['tasks'].'.php';
 		Factory::getApplication()->input->set('hidemainmenu', true);
-		$bar 		= Toolbar::getInstance('toolbar');
+
 		$user		= Factory::getUser();
 		$isNew		= ($this->item->id == 0);
 		$checkedOut	= !($this->item->checked_out == 0 || $this->item->checked_out == $user->get('id'));
@@ -60,12 +59,12 @@ class PhocaCartCpViewPhocacartParameterValue extends HtmlView
 
 		if (empty($this->item->id))  {
 			ToolbarHelper::cancel($this->t['task'].'.cancel', 'JTOOLBAR_CANCEL');
-		}
-		else {
+		} else {
 			ToolbarHelper::cancel($this->t['task'].'.cancel', 'JTOOLBAR_CLOSE');
 		}
 		ToolbarHelper::divider();
+		ToolbarHelper::inlinehelp();
 		ToolbarHelper::help( 'screen.'.$this->t['c'], true );
 	}
 }
-?>
+
