6f8bd3d32155e4f93c6b687a546983f7da009b61
n3t <n3t@n3t.cz>
2024-02-10 23:11:31 +0100

    Constants

---
+++ admin/libraries/src/Constants/GroupType.php
@@ -0,0 +1,15 @@
+<?php
+namespace Phoca\PhocaCart\Constants;
+
+final class GroupType
+{
+    public const User = 1;
+    public const Category = 2;
+    public const Product = 3;
+    public const ProductDiscount = 4;
+    public const Discount = 5;
+    public const Coupon = 6;
+    public const Shipping = 7;
+    public const Payment = 8;
+    public const FormField = 9;
+}
