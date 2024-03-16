f3974afe912a04a084d9d39d91939f56e08357cc
n3t <n3t@n3t.cz>
2024-03-16 03:55:54 +0100

    Admin - state and featured AJAX edit

---
+++ admin/libraries/src/Html/Grid/HtmlGridHelper.php
@@ -0,0 +1,112 @@
+<?php
+/* @package Joomla
+ * @copyright Copyright (C) Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @extension Phoca Extension
+ * @copyright Copyright (C) Jan Pavelka www.phoca.cz
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ */
+
+namespace Phoca\PhocaCart\Html\Grid;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\Utilities\ArrayHelper;
+
+defined('_JEXEC') or die;
+
+abstract class HtmlGridHelper
+{
+    private static function jsCommon(): void
+    {
+        Text::script('COM_PHOCACART_AJAX_CLOSE');
+        Text::script('COM_PHOCACART_AJAX_ERROR');
+    }
+
+    public static function featuredIcon(int $value, bool $enabled = true): string
+    {
+        $states = [
+            1  => ['COM_PHOCACART_TOGGLE_TO_UNFEATURE', 'COM_PHOCACART_FEATURED_PRODUCT', 'star featured'],
+            0  => ['COM_PHOCACART_TOGGLE_TO_FEATURE', 'COM_PHOCACART_UNFEATURED_PRODUCT', 'circle'],
+        ];
+
+        $state = ArrayHelper::getValue($states, $value, $states[0]);
+        $title  = $enabled ? $state[0] : $state[1];
+        $ariaid = uniqid('phfeatured');
+
+        $html = [];
+
+        $html[] = '<span class="tbody-icon jgrid"';
+        $html[] = ' aria-labelledby="' . $ariaid . '"';
+        $html[] = '>';
+        $html[] = LayoutHelper::render('joomla.icon.iconclass', ['icon' => $state[2]]);
+        $html[] = '</span>';
+        $html[] = '<div role="tooltip" id="' . $ariaid . '">' . Text::_($title) . '</div>';
+
+        return implode('', $html);
+    }
+
+    public static function featuredButton(string $controller, int $id, int $value, bool $enabled = true): string
+    {
+        if (!$enabled) {
+            return self::featuredIcon($value, $enabled);
+        }
+
+        self::jsCommon();
+
+        if ($value === 1) {
+            $newState = 0;
+        } else {
+            $newState = 1;
+        }
+
+        return '<a href="' . Route::_('index.php?option=com_phocacart&task=' . $controller . '.featured&format=json&id=' . $id). '" data-phajax="state=' . $newState . '">'
+            . self::featuredIcon($value, $enabled)
+            . '</a>';
+    }
+
+    public static function stateIcon(int $value, bool $enabled = true): string
+    {
+        $states = [
+            1  => ['JLIB_HTML_UNPUBLISH_ITEM', 'JPUBLISHED', 'publish'],
+            0  => ['JLIB_HTML_PUBLISH_ITEM', 'JUNPUBLISHED', 'unpublish'],
+            2  => ['JLIB_HTML_UNPUBLISH_ITEM', 'JARCHIVED', 'archive'],
+            -2 => ['JLIB_HTML_PUBLISH_ITEM', 'JTRASHED', 'trash'],
+        ];
+
+        $state = ArrayHelper::getValue($states, $value, $states[0]);
+        $title  = $enabled ? $state[0] : $state[1];
+        $ariaid = uniqid('phstate');
+
+        $html = [];
+
+        $html[] = '<span class="tbody-icon jgrid"';
+        $html[] = ' aria-labelledby="' . $ariaid . '"';
+        $html[] = '>';
+        $html[] = LayoutHelper::render('joomla.icon.iconclass', ['icon' => $state[2]]);
+        $html[] = '</span>';
+        $html[] = '<div role="tooltip" id="' . $ariaid . '">' . Text::_($title) . '</div>';
+
+        return implode('', $html);
+    }
+
+    public static function stateButton(string $controller, int $id, int $value, bool $enabled = true): string
+    {
+        if (!$enabled) {
+            return self::stateIcon($value, $enabled);
+        }
+
+        self::jsCommon();
+
+        if (in_array($value, [1, -2])) {
+            $newState = 0;
+        } else {
+            $newState = 1;
+        }
+
+        return '<a href="' . Route::_('index.php?option=com_phocacart&task=' . $controller . '.state&format=json&id=' . $id). '" data-phajax="state=' . $newState . '">'
+            . self::stateIcon($value, $enabled)
+            . '</a>';
+    }
+}
