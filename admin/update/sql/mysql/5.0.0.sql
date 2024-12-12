64a21b904be18e4a520c7a9e1b9f58532797bacb
n3t <n3t@n3t.cz>
2024-12-13 00:37:55 +0100

    Conflict resolution

--- admin/update/sql/mysql/5.0.0.sql
+++ admin/update/sql/mysql/5.0.0.sql
@@ -209,9 +209,6 @@ ALTER TABLE `#__phocacart_parameter_values_related` DROP INDEX `i_parameter_id`,
 
 ALTER TABLE `#__phocacart_wishlists` ADD COLUMN `language` CHAR(7) NOT NULL DEFAULT '';
 
-INSERT INTO `#__mail_templates` (`template_id`, `extension`, `language`, `subject`, `body`, `htmlbody`, `attachments`, `params`) VALUES
-    ('com_phocacart.watchdog', 'com_phocacart', '', 'COM_PHOCACART_EMAIL_WATCHDOG_SUBJECT', 'COM_PHOCACART_EMAIL_WATCHDOG_BODY', 'COM_PHOCACART_EMAIL_WATCHDOG_HTMLBODY', '', '{"tags":["user_name","user_username","user_email","product_title","product_sku","product_url","site_name","site_url"]}');
-
 ALTER TABLE `#__phocacart_categories` ADD COLUMN `description_bottom` TEXT AFTER `description`;
 ALTER TABLE `#__phocacart_categories_i18n` ADD COLUMN `description_bottom` TEXT AFTER `description`;
 
@@ -265,3 +262,25 @@ CREATE TABLE IF NOT EXISTS `#__phocacart_taxes_i18n` (
 
 -- 5.0.0 Beta83
 ALTER TABLE `#__phocacart_shipping_methods` ADD COLUMN `change_tax` tinyint(1) NOT NULL DEFAULT '0';
+
+INSERT IGNORE INTO `#__mail_templates` (`template_id`, `extension`, `language`, `subject`, `body`, `htmlbody`, `attachments`, `params`) VALUES
+    ('com_phocacart.watchdog', 'com_phocacart', '', 'COM_PHOCACART_EMAIL_WATCHDOG_SUBJECT', 'COM_PHOCACART_EMAIL_WATCHDOG_BODY', 'COM_PHOCACART_EMAIL_WATCHDOG_HTMLBODY', '', '{"tags":["user_name","user_username","user_email","products","product_title","product_sku","product_link","product_url","site_name","site_link","site_url"]}');
+INSERT IGNORE INTO `#__mail_templates` (`template_id`, `extension`, `language`, `subject`, `body`, `htmlbody`, `attachments`, `params`) VALUES
+    ('com_phocacart.question', 'com_phocacart', '', 'COM_PHOCACART_EMAIL_QUESTION_SUBJECT', 'COM_PHOCACART_EMAIL_QUESTION_BODY', 'COM_PHOCACART_EMAIL_QUESTION_HTMLBODY', '', '{"tags":["name","email","phone","product_title","product_long_title","product_sku","product_link","product_url","category_title","category_long_title","category_link","category_url","site_name","site_link","site_url"]}');
+INSERT IGNORE INTO `#__mail_templates` (`template_id`, `extension`, `language`, `subject`, `body`, `htmlbody`, `attachments`, `params`) VALUES
+    ('com_phocacart.question.admin', 'com_phocacart', '', 'COM_PHOCACART_EMAIL_QUESTION_ADMIN_SUBJECT', 'COM_PHOCACART_EMAIL_QUESTION_ADMIN_BODY', 'COM_PHOCACART_EMAIL_QUESTION_ADMIN_HTMLBODY', '', '{"tags":["name","email","phone","product_title","product_long_title","product_sku","product_link","product_url","category_title","category_long_title","category_link","category_url","site_name","site_link","site_url"]}');
+
+INSERT INTO `#__mail_templates` (`template_id`, `extension`, `language`, `subject`, `body`, `htmlbody`, `attachments`, `params`) VALUES
+    ('com_phocacart.submit_item', 'com_phocacart', '', 'COM_PHOCACART_EMAIL_SUBMIT_ITEM_SUBJECT', 'COM_PHOCACART_EMAIL_SUBMITE_ITEM_BODY', 'COM_PHOCACART_EMAIL_SUBMIT_ITEM_HTMLBODY', '', '{"tags":["user_name","user_username","user_email","products","product_title","product_sku","product_url","site_name","site_url"]}');
+
+
+INSERT INTO `#__mail_templates` (`template_id`, `extension`, `language`, `subject`, `body`, `htmlbody`, `attachments`, `params`)
+    SELECT
+        CONCAT('com_phocacart.order_status.', `id`),
+        'com_phocacart', '',
+        IF(`email_subject` = '', 'COM_PHOCACART_EMAIL_ORDER_STATUS_SUBJECT', `email_subject`),
+        'COM_PHOCACART_EMAIL_ORDER_STATUS_TEXTBODY',
+        IF(`email_send_format` in (0, 2), CONCAT(`email_text`, '{DOCUMENT}', `email_footer`), CONCAT(`email_text`, `email_footer`)),
+        '',
+        '{"tags":["user_name","user_username","user_email","document","product_title","product_sku","product_url","site_name","site_url"]}'
+    FROM `#__phocacart_order_statuses`;
