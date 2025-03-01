a804ccb7e960de2e0052a0976aa43b08d122a494
n3t <n3t@n3t.cz>
2025-03-01 17:50:00 +0100

    HTML emails not enabled warning

--- admin/libraries/src/Constants/EmailDocumentType.php
+++ admin/libraries/src/Constants/EmailDocumentType.php
@@ -3,7 +3,6 @@ namespace Phoca\PhocaCart\Constants;
 
 enum EmailDocumentType: int
 {
-    case None = 0;
     case Order = 1;
     case Invoice = 2;
     case DeliveryNote = 3;
---
+++ admin/libraries/src/Form/Field/HtmlEmailWarningField.php
@@ -0,0 +1,60 @@
+<?php
+/* @package Joomla
+ * @copyright Copyright (C) Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @extension Phoca Extension
+ * @copyright Copyright (C) Jan Pavelka www.phoca.cz
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ */
+
+namespace Phoca\PhocaCart\Form\Field;
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Form\Field\NoteField;
+
+/**
+ * Displays warning, when Joomla HTML mail templates are not enabled
+ *
+ * @since  5.0
+ */
+class HtmlEmailWarningField extends NoteField
+{
+    /**
+     * @since   5.0
+     * @inheritdoc
+     */
+    protected $type = 'HtmlEmailWarning';
+
+    private bool $htmlEmailEnabled = false;
+
+    /**
+     * @since 5.0.0
+     * @inheritdoc
+     */
+    public function setup(\SimpleXMLElement $element, $value, $group = null)
+    {
+        $mailParams = ComponentHelper::getParams('com_mails');
+        $this->htmlEmailEnabled = in_array($mailParams->get('mail_style', 'plaintext'), ['html', 'both']);
+
+        if (!$element['description'] ?? '') {
+            $element['description'] = 'COM_PHOCACART_WARNING_HTML_EMAILS_NOT_ENABLED';
+        }
+
+        if (!$element['class'] ?? '') {
+            $element['class'] = 'alert alert-warning w-100';
+        }
+
+        $result = parent::setup($element, $value, $group);
+
+        return $result;
+    }
+
+    protected function getLabel()
+    {
+        if ($this->htmlEmailEnabled) {
+            return '';
+        }
+
+        return parent::getLabel();
+    }
+}
